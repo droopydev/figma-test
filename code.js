e22b80ecbf734af88484e199136f4d74efe8ea28
droopydev <sufyazman@gmail.com>
2022-11-04 02:26:33 +0000

    Update Default Figma

--- code.js
+++ code.js
@@ -1,3 +1,1 @@
-throw new Error(
-  "This plugin template uses Typescript. Follow the instructions in `README.md` to generate `code.js`."
-);
+throw new Error("This plugin template uses Typescript. Follow the instructions in `README.md` to generate `code.js`.")
--- code.ts
+++ code.ts
@@ -1,21 +1,33 @@
-// This plugin creates 5 rectangles on the screen.
-const numberOfRectangles = 5
+// This plugin will open a window to prompt the user to enter a number, and
+// it will then create that many rectangles on the screen.
 
 // This file holds the main code for the plugins. It has access to the *document*.
-// You can access browser APIs such as the network by creating a UI which contains
-// a full browser environment (see documentation).
+// You can access browser APIs in the <script> tag inside "ui.html" which has a
+// full browser environment (see documentation).
 
-const nodes: SceneNode[] = [];
-for (let i = 0; i < numberOfRectangles; i++) {
-  const rect = figma.createRectangle();
-  rect.x = i * 150;
-  rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-  figma.currentPage.appendChild(rect);
-  nodes.push(rect);
-}
-figma.currentPage.selection = nodes;
-figma.viewport.scrollAndZoomIntoView(nodes);
+// This shows the HTML page in "ui.html".
+figma.showUI(__html__);
 
-// Make sure to close the plugin when you're done. Otherwise the plugin will
-// keep running, which shows the cancel button at the bottom of the screen.
-figma.closePlugin();
+// Calls to "parent.postMessage" from within the HTML page will trigger this
+// callback. The callback will be passed the "pluginMessage" property of the
+// posted message.
+figma.ui.onmessage = msg => {
+  // One way of distinguishing between different types of messages sent from
+  // your HTML page is to use an object with a "type" property like this.
+  if (msg.type === 'create-rectangles') {
+    const nodes: SceneNode[] = [];
+    for (let i = 0; i < msg.count; i++) {
+      const rect = figma.createRectangle();
+      rect.x = i * 150;
+      rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
+      figma.currentPage.appendChild(rect);
+      nodes.push(rect);
+    }
+    figma.currentPage.selection = nodes;
+    figma.viewport.scrollAndZoomIntoView(nodes);
+  }
+
+  // Make sure to close the plugin when you're done. Otherwise the plugin will
+  // keep running, which shows the cancel button at the bottom of the screen.
+  figma.closePlugin();
+};
